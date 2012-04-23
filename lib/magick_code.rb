5918682649c84bf0a8b99a71091d37431bf3c25b
Ankur Goel <ankurgel@gmail.com>
2012-04-23 19:22:42 +0530

    Resizer's library [status: pending]

---
+++ lib/magick_code.rb
@@ -0,0 +1,30 @@
+require "RMagick"
+include Magick
+
+class MagickClass
+    @@image_ext=%w[png jpg jpeg gif bmp timff].map{|x| "."+x}
+    attr_accessor :filesdetected, :parent_dir, :dir_location
+
+    def initialize(string)
+        if File.extname(string).size>1
+            @filedetected=string
+        else
+            @parent_dir=string
+            @filesdetected=[]
+            @dir_location=File.dirname(string)
+            extract_files
+        end
+    end
+    def extract_files
+        Dir.chdir(@parent_dir)
+        @@image_ext.each do |file_extension|
+            @filesdetected<<Dir['**/*'+file_extension]
+        end
+        @filesdetected.flatten!
+
+    end
+
+    def scale_to(x_percent)
+        ImageList.new(@file_location).scale(x_percent.to_f).write(File.basename(@file_location, File.extname(@file_location))+"_resized"+File.extname(@file_location))
+    end
+end
