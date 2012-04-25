c6a5513b099d377572ee3a399c7e9636af4ef776
Ankur Goel <ankurgel@gmail.com>
2012-04-25 15:17:09 +0530

    Starter!. Go Go

---
+++ run.rb
@@ -0,0 +1,12 @@
+require 'RMagick'
+require 'lib/magick_code'
+include Magick
+scale, *images=ARGV
+if (scale==nil||images==[])
+    puts "Usage instruction : ruby -I . run.rb percentage_to_scale_to location_of_image_or_directory"
+    raise ArgumentError, "Wrong number of arguments!"
+end
+images.each do |file_name| 
+    STDERR.puts "Now processing #{file_name}"
+    MagickClass.new(file_name).scale_to(scale)
+end
